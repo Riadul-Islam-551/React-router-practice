024627116b6dc4cca5413e302fa8a3b8c11d4eea
Riadul Islam <riadul551@gmail.com>
2025-10-07 23:34:51 +0600

    implement the nested router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/root/Root.jsx";
+import Mobiles from "./components/mobiles/Mobiles.jsx";
+import Laptops from "./components/laptops/Laptops.jsx";
+import Home from "./components/home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello router</div>,
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "laptops", Component: Laptops },
+    ],
   },
   {
     path: "about",
