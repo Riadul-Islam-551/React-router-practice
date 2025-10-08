d0fc96c6ce4b4ae82c755ba9ffa9a450bffc5a95
Riadul Islam <riadul551@gmail.com>
2025-10-08 09:20:52 +0600

    implement some nav links to the header file and make a footer component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,11 @@ const Header = () => {
   return (
     <div>
       <h3>This is Header</h3>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/laptops">Laptop</a>
+        <a href="/mobiles">Mobiles</a>
+      </nav>
     </div>
   )
 }
--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
