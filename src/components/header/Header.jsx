4a109ec888b7036fed2f37d9fc5d3e5c03d90337
Riadul Islam <riadul551@gmail.com>
2025-10-08 09:51:52 +0600

    implement navlinks in nav as anchore tag and add active class in navigation link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 const Header = () => {
   return (
     <div>
       <h3>This is Header</h3>
-      <nav>
-        <Link to="/" className='p-2'>Home</Link>
-        <Link to="/laptops" className='p-2'>Laptops</Link>
-        <Link to="/mobiles" className='p-2'>Mobiles</Link>
+      <nav className='my-9 '>
+        <NavLink to="/" className='p-2'>Home</NavLink>
+        <NavLink to="/laptops" className='p-2'>Laptops</NavLink>
+        <NavLink to="/mobiles" className='p-2'>Mobiles</NavLink>
       </nav>
     </div>
   )
